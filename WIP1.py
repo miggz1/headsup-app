48dec5d719a0493ac8cb1814e45b5c8b5b248efe
Miguel <mrod1018@gmail.com>
2025-06-17 21:29:25 -0700

    Update WIP1.py with my changes

--- WIP1.py
+++ WIP1.py
@@ -1,7 +1,10 @@
-print("Hello world")
-
 def calculate_session_total(min, sessions):
     sessionTotal = min + sessions
     print(sessionTotal)
 
-calculate_session_total(5, 25)
+
+print("Hello world" + ", I am here to save you.")
+
+
+
+
